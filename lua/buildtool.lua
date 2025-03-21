3c55eec51c4c85f59749092651c2621f1d75b3c1
Elijah Hantman <elihantman@gmail.com>
2025-03-20 23:24:11 -0700

    outputs signal and exit code

--- lua/buildtool.lua
+++ lua/buildtool.lua
@@ -14,6 +14,52 @@ local run_files = {
 }
 
 local buf = nil
+local win = nil
+
+local function openwin()
+    local valid = nil
+    for _, handle in ipairs(vim.api.nvim_list_wins()) do
+        if handle == win then
+            valid = 1
+            break
+        end
+    end
+
+    if not valid then
+        win = vim.api.nvim_open_win(buf, false, {
+            split = "right",
+            win = 0
+        })
+    end
+end
+
+local function buildoutput(obj)
+    local lines = {}
+
+    local formatbreak = false
+
+    if obj.stdout then
+        formatbreak = true
+        for s in obj.stdout:gmatch("[^\r\n]+") do
+            table.insert(lines, s)
+        end
+    end
+
+    if obj.stderr then
+        formatbreak = true
+        for s in obj.stderr:gmatch("[^\r\n]+") do
+            table.insert(lines, s)
+        end
+    end
+
+    if formatbreak then
+        table.insert(lines, "")
+    end
+
+    table.insert(lines, string.format("Signal: %d", obj.signal))
+    table.insert(lines, string.format("Code: %d", obj.code))
+    return lines
+end
 
 local function Build()
     local dir = vim.fn.getcwd()
@@ -29,16 +75,11 @@ local function Build()
         return 
     end
     
-    local obj = vim.system({filetorun}, { text = true }, on_exit):wait()
-    vim.api.nvim_open_win(buf, false, {
-        split = "right",
-        win = 0
-    })
+    local obj = vim.system({filetorun}, { text = true }, on_exit):wait(1000)
+    openwin()
 
-    local lines = {}
-    for s in obj.stdout:gmatch("[^\r\n]+") do
-        table.insert(lines, s)
-    end
+    local lines = buildoutput(obj)
+
     vim.api.nvim_buf_set_lines(buf, 0, -1, true, lines)
 end
 
@@ -56,16 +97,10 @@ local function Run()
         return 
     end
     
-    local obj = vim.system({filetorun}, { text = true }, on_exit):wait()
-    vim.api.nvim_open_win(buf, false, {
-        split = "right",
-        win = 0
-    })
+    local obj = vim.system({filetorun}, { text = true }, on_exit):wait(1000)
+    openwin()
+    local lines = buildoutput(obj)
 
-    local lines = {}
-    for s in obj.stdout:gmatch("[^\r\n]+") do
-        table.insert(lines, s)
-    end
     vim.api.nvim_buf_set_lines(buf, 0, -1, true, lines)
 end
 
@@ -79,5 +114,14 @@ local function setup()
     vim.api.nvim_create_user_command('BTRun', Run, {})
 end
 
+local function Clear()
+    for _, handle in ipairs(vim.api.nvim_list_wins()) do
+        if win == handle then
+            vim.api.nvim_win_close(win, true)
+            break
+        end
+    end
+end
 
-return { setup=setup, build=Build, run=Run }
+
+return { setup=setup, build=Build, run=Run, clear=Clear }
